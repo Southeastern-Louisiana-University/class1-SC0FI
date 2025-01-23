280d689e6c13278978634e016ae195f1df32e434
SC0FI <Maddoxjacob64@proton.me>
2025-01-23 12:16:33 -0600

    Attempt #1 1/23/25

---
+++ src/start.java
@@ -0,0 +1,20 @@
+package src;
+
+public class start {
+    public static void main (String args[]) {
+        double score = 0;
+        if (score >= 60) {
+            System.out.println("D");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else if (score >= 80) {
+            System.out.println("B");
+        }
+        else if (score >= 90) {
+            System.out.println("A");
+        }
+        }
+
+}
