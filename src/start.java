d14e9567d2948af0afbcd0d5c02a63024d77265e
SC0FI - Laptop <maddoxjacob64@proton.me>
2025-01-30 11:21:12 -0600

    fixing

--- src/start.java
+++ src/start.java
@@ -2,18 +2,21 @@ package src;
 
 public class start {
     public static void main (String args[]) {
-        double score = 0;
-        if (score >= 60) {
-            System.out.println("D");
-        }
-        else if (score >= 70) {
-            System.out.println("C");
+        double score = 100;
+        if (score >= 90) {
+            System.out.println("A");
         }
         else if (score >= 80) {
             System.out.println("B");
         }
-        else if (score >= 90) {
-            System.out.println("A");
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else if (score >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
         }
         }
 
